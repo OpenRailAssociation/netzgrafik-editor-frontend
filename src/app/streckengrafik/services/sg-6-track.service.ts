660a67b1b08809252da9514cd6494791ad0a14c0
Adrian Egli <adrian.egli@sbb.ch>
2025-10-14 17:28:35 +0200

    Math.max replaced by faster variant

--- src/app/streckengrafik/services/sg-6-track.service.ts
+++ src/app/streckengrafik/services/sg-6-track.service.ts
@@ -189,7 +189,9 @@ export class Sg6TrackService implements OnDestroy {
     sectionData.forEach((d) => {
       const item: SgTrainrunItem = d.item;
       const travelTime = item.arrivalTime - item.departureTime;
-      nDistanceCells = Math.max(nDistanceCells, Math.round(travelTime));
+      if (nDistanceCells < travelTime ) {
+        nDistanceCells =  Math.round(travelTime);
+      }
     });
     return nDistanceCells;
   }
@@ -337,11 +339,13 @@ export class Sg6TrackService implements OnDestroy {
         dataMatrix[idx],
         headwayTime,
         nTimeCells,
-      );
+      ); 
+
       const val = tracksMatrix[idx];
       if (val < localMax) {
         tracksMatrix[idx] = localMax;
       }
+
     }
   }
 
