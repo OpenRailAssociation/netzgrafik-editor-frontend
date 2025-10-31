c2757838d70f63a7ae31c4ceea7d31b2788afbfa
Clara Ni <clara.ni@outlook.fr>
2025-10-31 15:31:24 +0100

    feat(times): correctly update arrival/departure/travel times when a node is masked
    The behavior is pretty much the same as when a node is a non-stop node, except
    when updating the travel time, because we need to take into account the stop
    times of the reduced nodes.
    
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- src/app/services/util/trainrun.iterator.ts
+++ src/app/services/util/trainrun.iterator.ts
@@ -287,9 +287,13 @@ export class BackwardTrainrunIterator extends TrainrunIterator {
   }
 }
 
-export class NonStopTrainrunIterator extends TrainrunIterator {
+/** Iterate on the trainrun sections until we find a node which is a stop of the trainrun and not collapsed */
+export class NextExpandedStopIterator extends TrainrunIterator {
   public next(): TrainrunSectionNodePair {
-    if (!this.pointerElement.node.isNonStop(this.pointerElement.trainrunSection)) {
+    if (
+      !this.pointerElement.node.isNonStop(this.pointerElement.trainrunSection) &&
+      !this.pointerElement.node.getIsCollapsed()
+    ) {
       // The trainrun has a stop and break the forward iteration
       this.currentElement = this.pointerElement;
       this.pointerElement = new TrainrunSectionNodePair(undefined, undefined);
@@ -299,9 +303,12 @@ export class NonStopTrainrunIterator extends TrainrunIterator {
   }
 }
 
-export class BackwardNonStopTrainrunIterator extends BackwardTrainrunIterator {
+export class BackwardNextExpandedStopIterator extends BackwardTrainrunIterator {
   public next(): TrainrunSectionNodePair {
-    if (!this.pointerElement.node.isNonStop(this.pointerElement.trainrunSection)) {
+    if (
+      !this.pointerElement.node.isNonStop(this.pointerElement.trainrunSection) &&
+      !this.pointerElement.node.getIsCollapsed()
+    ) {
       // The trainrun has a stop and break the backward iteration
       this.currentElement = this.pointerElement;
       this.pointerElement = new TrainrunSectionNodePair(undefined, undefined);
