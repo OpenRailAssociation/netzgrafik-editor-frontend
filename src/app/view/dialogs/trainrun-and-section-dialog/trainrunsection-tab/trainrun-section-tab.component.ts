c4aeaa2655887c5bf0af2cd012cb97bbbf808e9f
Clara Ni <clara.ni@outlook.fr>
2025-10-31 15:31:24 +0100

    feat(times): correctly update arrival/departure/travel times when a node is masked
    The behavior is pretty much the same as when a node is a non-stop node, except
    when updating the travel time, because we need to take into account the stop
    times of the reduced nodes.
    
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>

--- src/app/view/dialogs/trainrun-and-section-dialog/trainrunsection-tab/trainrun-section-tab.component.ts
+++ src/app/view/dialogs/trainrun-and-section-dialog/trainrunsection-tab/trainrun-section-tab.component.ts
@@ -93,7 +93,7 @@ export class TrainrunSectionTabComponent implements AfterViewInit, OnDestroy {
     const firstTrainrunSection = this.trainrunService.getFirstNonStopTrainrunSection(
       this.selectedTrainrunSection,
     );
-    const iterator = this.trainrunService.getNonStopIterator(
+    const iterator = this.trainrunService.getNextExpandedStopIterator(
       firstTrainrunSection.getSourceNode(),
       firstTrainrunSection,
     );
