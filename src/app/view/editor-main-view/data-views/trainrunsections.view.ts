532ae6f4d4265168bfa7b02ac0a59880ef003f7b
Simon Ser <contact@emersion.fr>
2025-11-05 11:15:56 +0100

    refactor: Use all trainrun sections in TrainrunSectionViewObject.generateKey()
    We were only using the first trainrun section's path here.
    
    Expose a getPath() method to get a view object's path. We need to
    call it from inside generateKey() so drop the static attribute.
    
    While at it, make the generateKey() helper function private because
    it shouldn't be called from elsewhere.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- src/app/view/editor-main-view/data-views/trainrunsections.view.ts
+++ src/app/view/editor-main-view/data-views/trainrunsections.view.ts
@@ -774,6 +774,7 @@ export class TrainrunSectionsView {
       }
       case TrainrunSectionText.TrainrunSectionTravelTime:
       case TrainrunSectionText.TrainrunSectionBackwardTravelTime: {
+        const isForward = textElement === TrainrunSectionText.TrainrunSectionTravelTime;
         const data = TrainrunSectionsView.getFormattedDisplayText(trainrunSection, textElement);
         if (data !== undefined) {
           return data;
@@ -781,9 +782,7 @@ export class TrainrunSectionsView {
         return TrainrunSectionsView.extractTravelTime(
           trainrunSection,
           editorView,
-          textElement === TrainrunSectionText.TrainrunSectionTravelTime
-            ? "sourceToTarget"
-            : "targetToSource",
+          isForward ? "sourceToTarget" : "targetToSource",
         );
       }
       case TrainrunSectionText.TrainrunSectionName:
