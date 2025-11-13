dcfaea2c47a94cca37097e0a4d101e98158be3ed
Alexis Jacomy <alexis.jacomy@gmail.com>
2025-11-13 12:52:33 +0100

    front: improves global nodes management panel
    This commit handles various feedbacks from reviewers on PR:
    https://github.com/OpenRailAssociation/netzgrafik-editor-frontend/pull/613
    
    Details:
    - Fixes some FR translations
    - Renames "visible nodes" as "matching nodes"
    - Caches the matching nodes array
    - Adds a confirmation modal when clicking the global checkbox

--- src/app/view/editor-edit-tools-view-component/global-nodes-management/global-nodes-management.component.ts
+++ src/app/view/editor-edit-tools-view-component/global-nodes-management/global-nodes-management.component.ts
@@ -5,6 +5,10 @@ import {SbbInput} from "@sbb-esta/angular/input";
 
 import {NodeService} from "../../../services/data/node.service";
 import {I18nModule} from "../../../core/i18n/i18n.module";
+import {Node} from "../../../models/node.model";
+import {DataService} from "../../../services/data/data.service";
+import {UiInteractionService} from "../../../services/ui/ui.interaction.service";
+import {ConfirmationDialogParameter} from "../../dialogs/confirmation-dialog/confirmation-dialog.component";
 
 function normalizeStr(str: string): string {
   return str
@@ -23,32 +27,39 @@ function normalizeStr(str: string): string {
 })
 export class GlobalNodesManagementComponent {
   query: string;
+  allNodes: Node[];
+  matchingNodes: Node[];
 
-  constructor(private nodeService: NodeService) {
+  constructor(
+    private dataService: DataService,
+    private nodeService: NodeService,
+    private uiInteractionService: UiInteractionService,
+  ) {
     this.query = "";
+    this.allNodes = this.nodeService.getNodes();
+    this.nodeService.nodes.subscribe((nodes) => this.updateState({nodes}));
   }
 
-  toggleIsCollapsedOnAllVisibleNodes(newValue: boolean) {
-    this.getVisibleNodes().forEach((node) => {
-      node.setIsCollapsed(newValue);
-    });
-  }
+  updateState({nodes = this.allNodes, query = this.query}: {nodes?: Node[]; query?: string}) {
+    // Save state locally
+    this.query = query;
+    this.allNodes = nodes;
 
-  getVisibleNodes() {
-    const allNodes = this.nodeService.getNodes();
     const normalizedQuery = normalizeStr(this.query);
-    if (!normalizedQuery) return allNodes;
 
-    return allNodes.filter(
-      (node) =>
-        normalizeStr(node.getFullName()).includes(normalizedQuery) ||
-        normalizeStr(node.getBetriebspunktName()).includes(normalizedQuery),
-    );
+    this.matchingNodes = normalizedQuery
+      ? this.allNodes.filter(
+          (node) =>
+            normalizeStr(node.getFullName()).includes(normalizedQuery) ||
+            normalizeStr(node.getBetriebspunktName()).includes(normalizedQuery),
+        )
+      : this.allNodes;
   }
+
   getGlobalCheckboxStatus(): boolean | undefined {
     let allCollapsed = true;
     let noneCollapsed = true;
-    this.getVisibleNodes().every((node) => {
+    this.matchingNodes.every((node) => {
       const isCollapsed = node.getIsCollapsed();
       allCollapsed = allCollapsed && isCollapsed;
       noneCollapsed = noneCollapsed && !isCollapsed;
@@ -60,5 +71,43 @@ export class GlobalNodesManagementComponent {
     if (allCollapsed) return false;
     if (noneCollapsed) return true;
     return undefined;
+  }
+
+  toggleIsCollapsed(node: Node, isCollapsed: boolean) {
+    node.setIsCollapsed(isCollapsed);
+    this.dataService.triggerViewUpdate();
+  }
+  onClickGlobalCheckbox(event: MouseEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const currentGlobalCheckboxStatus = this.getGlobalCheckboxStatus();
+    const newCheckboxStatus = !currentGlobalCheckboxStatus;
+    const newIsCollapsed = !newCheckboxStatus;
+
+    const allNodesImpacted = this.allNodes.length === this.matchingNodes.length;
+    const impactedNodesCount = this.matchingNodes.length;
+
+    const apply = () => {
+      this.matchingNodes.forEach((node) => {
+        node.setIsCollapsed(newIsCollapsed);
+      });
+    };
+
+    const dialogTitle = $localize`:@@app.view.editor-edit-tools-view-component.global-nodes-management:Global nodes management`;
+    const dialogContent = newIsCollapsed
+      ? allNodesImpacted
+        ? $localize`:@@app.view.editor-edit-tools-view-component.confirm-collapse-all:Are you sure you want to collapse all nodes?`
+        : $localize`:@@app.view.editor-edit-tools-view-component.confirm-collapse-matching:Are you sure you want to collapse the ${impactedNodesCount}:count: nodes matching "${this.query}:query:"?`
+      : allNodesImpacted
+        ? $localize`:@@app.view.editor-edit-tools-view-component.confirm-expand-all:Are you sure you want to expand all nodes?`
+        : $localize`:@@app.view.editor-edit-tools-view-component.confirm-expand-matching:Are you sure you want to expand the ${impactedNodesCount}:count: nodes matching "${this.query}:query:"?`;
+    const confirmationDialogParameter = new ConfirmationDialogParameter(dialogTitle, dialogContent);
+
+    this.uiInteractionService
+      .showConfirmationDiagramDialog(confirmationDialogParameter)
+      .subscribe((confirmed: boolean) => {
+        if (confirmed) apply();
+      });
   }
 }
