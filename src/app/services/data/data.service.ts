OpenRailAssociation/netzgrafik-editor-frontend

commit 435f1c66b8ede96870c312fac2dca55ea4fa30f4

import: handle importing old files with numberOfStops

src/app/services/data/data.service.ts
@@ -22,6 +22,7 @@ import {DataMigration} from "../../utils/data-migration";
 import {FilterService} from "../ui/filter.service";
 import {NetzgrafikColoringService} from "./netzgrafikColoring.service";
 import {Trainrun} from "src/app/models/trainrun.model";
+import {Vec2D} from "src/app/utils/vec2D";
 
 export class NetzgrafikLoadedInfo {
   constructor(
@@ -30,9 +31,7 @@ export class NetzgrafikLoadedInfo {
   ) {}
 }
 
-@Injectable({
-  providedIn: "root",
-})
+@Injectable({providedIn: "root"})
 export class DataService implements OnDestroy {
   private netzgrafikDtoStore: {netzgrafikDto: NetzgrafikDto} = {
     netzgrafikDto: NetzgrafikDefault.getDefaultNetzgrafik(),
@@ -99,12 +98,37 @@ export class DataService implements OnDestroy {
       this.trainrunSectionService.enforceConsistentSectionDirection(trainrun.getId());
     });
 
+    this.replaceLegacyNumberOfStopsWithRealNodes(netzgrafikDto);
+
     // This must be done due of the bug fix - ensure that each resource object
     // is used in the Netzgrafik
     // https://github.com/OpenRailAssociation/netzgrafik-editor-frontend/issues/522
     this.ensureAllResourcesLinkedToNetzgrafikObjects();
 
     this.netzgrafikLoadedInfoSubject.next(new NetzgrafikLoadedInfo(false, preview));
+  }
+
+  replaceLegacyNumberOfStopsWithRealNodes(netzgrafikDto: NetzgrafikDto) {
+    for (const trainrunSection of netzgrafikDto.trainrunSections) {
+      let currentSection = trainrunSection;
+      for (let i = 0; i < trainrunSection.numberOfStops; i++) {
+        const newNode = this.nodeService.addEmptyNode();
+        const {existingTrainRunSection, newTrainRunSection} =
+          this.trainrunSectionService.replaceIntermediateStopWithNode(
+            currentSection.id,
+            0,
+            newNode.getId(),
+          );
+        const sourceNode = existingTrainRunSection.getSourceNode();
+        const targetNode = newTrainRunSection.getTargetNode();
+        const interpolatedPosition = new Vec2D(
+          sourceNode.getPositionX() + (targetNode.getPositionX() - sourceNode.getPositionX()) * 0.5,
+          sourceNode.getPositionY() + (targetNode.getPositionY() - sourceNode.getPositionY()) * 0.5,
+        );
+        newNode.setPosition(interpolatedPosition.getX(), interpolatedPosition.getY());
+        currentSection = newTrainRunSection.getDto();
+      }
+    }
   }
 
   insertCopyNetzgrafikDto(netzgrafikDto: NetzgrafikDto, enforceUpdate = true) {

src/app/services/data/node.service.ts
@@ -38,9 +38,7 @@ import {
   TrainrunOperation,
 } from "../../models/operation.model";
 
-@Injectable({
-  providedIn: "root",
-})
+@Injectable({providedIn: "root"})
 export class NodeService implements OnDestroy {
   // Description of observable data service: https://coryrylan.com/blog/angular-observable-data-services
   nodesSubject = new BehaviorSubject<Node[]>([]);
@@ -252,6 +250,16 @@ export class NodeService implements OnDestroy {
     if (enforceUpdate) {
       this.nodesUpdated();
     }
+    this.operation.emit(new NodeOperation(OperationType.create, node));
+    return node;
+  }
+
+  addEmptyNode(): Node {
+    const node: Node = new Node();
+    node.setIsCollapsed(true);
+    const resource: Resource = this.resourceService.createAndGetResource();
+    node.setResourceId(resource.getId());
+    this.nodesStore.nodes.push(node);
     this.operation.emit(new NodeOperation(OperationType.create, node));
     return node;
   }

src/app/sample-netzgrafik/netzgrafik.default.spec.ts
@@ -1,5 +1,6 @@
 import netzgrafikDefaultJson from "./netzgrafik_default.json";
 import {NetzgrafikDefault} from "./netzgrafik.default";
+import {NetzgrafikTestData} from "./test-data/netzgrafik";
 import {DataService} from "../services/data/data.service";
 import {LogPublishersService} from "../logger/log.publishers.service";
 import {LogService} from "../logger/log.service";
@@ -63,21 +64,116 @@ describe("NetzgrafikDefault", () => {
     );
   });
 
-  it("should load and serialize default netzgrafikDto (no trainruns) without changes", () => {
-    const inputDto = NetzgrafikDefault.getDefaultNetzgrafik();
+  // TODO: broken until the numberOfStops migration is completely done
+  // need to be replaced with tests importing a graph without any numberOfStops
+  describe("normal case", () => {
+    // needs to be updated to new format (no longer numberOfStops but the complete graph nodes + links)
+    it("should load and serialize default netzgrafikDto (no trainruns) without changes", () => {
+      const inputDto = NetzgrafikDefault.getDefaultNetzgrafik();
 
-    dataService.loadNetzgrafikDto(inputDto);
+      dataService.loadNetzgrafikDto(inputDto);
 
-    const outputJson = dataService.getNetzgrafikDto();
-    expect(JSON.stringify(inputDto, null, 2)).toEqual(JSON.stringify(outputJson, null, 2));
+      const outputJson = dataService.getNetzgrafikDto();
+      expect(JSON.stringify(inputDto, null, 2)).toEqual(JSON.stringify(outputJson, null, 2));
+    });
+
+    it("should load and serialize demo netzgrafikDto (complete variant) without changes", () => {
+      const inputDto = NetzgrafikDefault.getNetzgrafikDemoStandaloneGithub();
+
+      dataService.loadNetzgrafikDto(inputDto);
+
+      const outputJson = dataService.getNetzgrafikDto();
+      expect(JSON.stringify(inputDto, null, 2)).toEqual(JSON.stringify(outputJson, null, 2));
+    });
   });
 
-  it("should load and serialize demo netzgrafikDto (complete variant) without changes", () => {
-    const inputDto = NetzgrafikDefault.getNetzgrafikDemoStandaloneGithub();
+  describe("handling old files with numberOfStops", () => {
+    describe("2 nodes", () => {
+      // A -> B has 1 stop
+      it("should replace the numberOfStops: 1 with an equivalent graph portion", () => {
+        const inputDto = NetzgrafikTestData.getLegacyNtezgrafik2Nodes();
 
-    dataService.loadNetzgrafikDto(inputDto);
+        dataService.loadNetzgrafikDto(inputDto);
 
-    const outputJson = dataService.getNetzgrafikDto();
-    expect(JSON.stringify(inputDto, null, 2)).toEqual(JSON.stringify(outputJson, null, 2));
+        const outputJson = dataService.getNetzgrafikDto();
+
+        // we expect to have a new node between A and B
+        // the existing trainrunSection A -> B to now be A -> NEW
+        // and a new trainrunSection NEW -> B
+        const newNode = outputJson.nodes[2];
+        expect(newNode.betriebspunktName).toBe("NEW");
+        expect(newNode.positionX).toBe(1488);
+        expect(newNode.positionY).toBe(512);
+        expect(outputJson.trainrunSections[0].sourceNodeId).toBe(11);
+        expect(outputJson.trainrunSections[0].targetNodeId).toBe(newNode.id);
+        expect(outputJson.trainrunSections[1].sourceNodeId).toBe(newNode.id);
+        expect(outputJson.trainrunSections[1].targetNodeId).toBe(12);
+        expect(outputJson.trainrunSections[0].numberOfStops).toBe(0);
+        expect(outputJson.trainrunSections[1].numberOfStops).toBe(0);
+      });
+      it("should replace the numberOfStops: 3 with an equivalent graph portion", () => {
+        const inputDto = NetzgrafikTestData.getLegacyNtezgrafik2Nodes();
+        inputDto.trainrunSections[0].numberOfStops = 3;
+
+        dataService.loadNetzgrafikDto(inputDto);
+        // before
+        // A ------ B
+        // after
+        // A ------ C ------ D ------ E ------ B
+        const outputJson = dataService.getNetzgrafikDto();
+        const newNode1 = outputJson.nodes[2];
+        expect(newNode1.betriebspunktName).toBe("NEW");
+        expect(newNode1.positionX).toBe(1488);
+        expect(newNode1.positionY).toBe(512);
+        const newNode2 = outputJson.nodes[3];
+        expect(newNode2.betriebspunktName).toBe("NEW");
+        expect(newNode2.positionX).toBe(1576);
+        expect(newNode2.positionY).toBe(512);
+        const newNode3 = outputJson.nodes[4];
+        expect(newNode3.betriebspunktName).toBe("NEW");
+        expect(newNode3.positionX).toBe(1620);
+        expect(newNode3.positionY).toBe(512);
+        expect(outputJson.trainrunSections[0].sourceNodeId).toBe(11);
+        expect(outputJson.trainrunSections[0].targetNodeId).toBe(newNode1.id);
+        expect(outputJson.trainrunSections[1].sourceNodeId).toBe(newNode1.id);
+        expect(outputJson.trainrunSections[1].targetNodeId).toBe(newNode2.id);
+        expect(outputJson.trainrunSections[2].sourceNodeId).toBe(newNode2.id);
+        expect(outputJson.trainrunSections[2].targetNodeId).toBe(newNode3.id);
+        expect(outputJson.trainrunSections[3].sourceNodeId).toBe(newNode3.id);
+        expect(outputJson.trainrunSections[3].targetNodeId).toBe(12);
+        expect(outputJson.trainrunSections[0].numberOfStops).toBe(0);
+        expect(outputJson.trainrunSections[1].numberOfStops).toBe(0);
+        expect(outputJson.trainrunSections[2].numberOfStops).toBe(0);
+        expect(outputJson.trainrunSections[3].numberOfStops).toBe(0);
+      });
+    });
+    describe("3 nodes", () => {
+      // A -> B -> C
+      // each trainrunSection with 1 stop
+      it("should replace the numberOfStops: 1 with an equivalent graph portion for each section", () => {
+        const inputDto = NetzgrafikTestData.getLegacyNtezgrafik3Nodes();
+
+        dataService.loadNetzgrafikDto(inputDto);
+
+        const outputJson = dataService.getNetzgrafikDto();
+
+        const newNode1 = outputJson.nodes[3];
+        expect(newNode1.betriebspunktName).toBe("NEW");
+        expect(newNode1.positionX).toBe(1488);
+        expect(newNode1.positionY).toBe(512);
+        const newNode2 = outputJson.nodes[4];
+        expect(newNode2.betriebspunktName).toBe("NEW");
+        expect(newNode2.positionX).toBe(1808);
+        expect(newNode2.positionY).toBe(544);
+        expect(outputJson.trainrunSections[0].sourceNodeId).toBe(11);
+        expect(outputJson.trainrunSections[0].targetNodeId).toBe(newNode1.id);
+        expect(outputJson.trainrunSections[2].sourceNodeId).toBe(newNode1.id);
+        expect(outputJson.trainrunSections[2].targetNodeId).toBe(12);
+        expect(outputJson.trainrunSections[1].sourceNodeId).toBe(12);
+        expect(outputJson.trainrunSections[1].targetNodeId).toBe(newNode2.id);
+        expect(outputJson.trainrunSections[3].sourceNodeId).toBe(newNode2.id);
+        expect(outputJson.trainrunSections[3].targetNodeId).toBe(13);
+      });
+    });
   });
 });

src/app/sample-netzgrafik/test-data/netzgrafik.ts
@@ -1,0 +1,13 @@
+import {NetzgrafikDto} from "../../data-structures/business.data.structures";
+import netzgrafikLegacy2NodesJson from "./2-nodes-legacy.json";
+import netzgrafikLegacy3NodesJson from "./3-nodes-legacy.json";
+
+export class NetzgrafikTestData {
+  // old exported json data, still using numberOfStops trainrunSections format
+  static getLegacyNtezgrafik2Nodes(): NetzgrafikDto {
+    return JSON.parse(JSON.stringify(netzgrafikLegacy2NodesJson)) as NetzgrafikDto;
+  }
+  static getLegacyNtezgrafik3Nodes(): NetzgrafikDto {
+    return JSON.parse(JSON.stringify(netzgrafikLegacy3NodesJson)) as NetzgrafikDto;
+  }
+}

src/app/services/data/trainrunsection.service.ts
@@ -21,6 +21,7 @@ import {takeUntil} from "rxjs/operators";
 import {FilterService} from "../ui/filter.service";
 import {TrainrunSectionNodePair} from "../util/trainrun.iterator";
 import {Operation, OperationType, TrainrunOperation} from "../../models/operation.model";
+import {Vec2D} from "../../utils/vec2D";
 
 interface DepartureAndArrivalTimes {
   nodeFromDepartureTime: number;
@@ -34,16 +35,12 @@ export interface InformSelectedTrainrunClick {
   open: boolean;
 }
 
-@Injectable({
-  providedIn: "root",
-})
+@Injectable({providedIn: "root"})
 export class TrainrunSectionService implements OnDestroy {
   // Description of observable data service: https://coryrylan.com/blog/angular-observable-data-services
   trainrunSectionsSubject = new BehaviorSubject<TrainrunSection[]>([]);
   readonly trainrunSections = this.trainrunSectionsSubject.asObservable();
-  trainrunSectionsStore: {trainrunSections: TrainrunSection[]} = {
-    trainrunSections: [],
-  }; // store the data in memory
+  trainrunSectionsStore: {trainrunSections: TrainrunSection[]} = {trainrunSections: []}; // store the data in memory
 
   readonly operation = new EventEmitter<Operation>();
 
@@ -936,13 +933,14 @@ export class TrainrunSectionService implements OnDestroy {
     });
   }
 
+  // this function is no longer used for its original purpose (drag a node that only existed inside numberOfStops and create it inside the real graph)
   replaceIntermediateStopWithNode(trainrunSectionId: number, stopIndex: number, nodeId: number) {
     const trainrunSection1 = this.getTrainrunSectionFromId(trainrunSectionId);
     if (
       trainrunSection1.getSourceNodeId() === nodeId ||
       trainrunSection1.getTargetNodeId() === nodeId
     ) {
-      return;
+      return {};
     }
     const origTravelTime = trainrunSection1.getTravelTime();
     const trainrunSection2 = this.copyTrainrunSection(
@@ -1048,6 +1046,10 @@ export class TrainrunSectionService implements OnDestroy {
     this.nodeService.connectionsUpdated();
     this.nodeService.nodesUpdated();
     this.trainrunSectionsUpdated();
+    return {
+      existingTrainRunSection: trainrunSection1,
+      newTrainRunSection: trainrunSection2,
+    };
   }
 
   setWarningOnNode(

commit 1ee6c70f8537648de68b6665eb8ecd4881bc6706

import: set node position before calling replaceIntermediateStopWithNode()
replaceIntermediateStopWithNode() recomputes a bunch of transition
position stuff. At import time, tt was working with incorrect node
positions, routing everything to the (0, 0) position. This causes
transition positions to be mis-rendered.

Instead, set the node position at initialization, before calling
replaceIntermediateStopWithNode(). That way the function can work
with the final node position. As a bonus, the "new node" event is
fired with the correct node position.

Signed-off-by: Simon Ser <contact@emersion.fr>
Fixes: 435f1c66b8ed ("import: handle importing old files with numberOfStops")

src/app/services/data/data.service.ts
@@ -112,20 +112,28 @@ export class DataService implements OnDestroy {
     for (const trainrunSection of netzgrafikDto.trainrunSections) {
       let currentSection = trainrunSection;
       for (let i = 0; i < trainrunSection.numberOfStops; i++) {
-        const newNode = this.nodeService.addEmptyNode();
+        const oldTrainrunSection = this.trainrunSectionService.getTrainrunSectionFromId(
+          currentSection.id,
+        );
+        const sourceNode = oldTrainrunSection.getSourceNode();
+        const targetNode = oldTrainrunSection.getTargetNode();
+        const interpolatedPosition = new Vec2D(
+          sourceNode.getPositionX() + (targetNode.getPositionX() - sourceNode.getPositionX()) * 0.5,
+          sourceNode.getPositionY() + (targetNode.getPositionY() - sourceNode.getPositionY()) * 0.5,
+        );
+
+        const newNode = this.nodeService.addEmptyNode(
+          interpolatedPosition.getX(),
+          interpolatedPosition.getY(),
+        );
+
         const {existingTrainRunSection, newTrainRunSection} =
           this.trainrunSectionService.replaceIntermediateStopWithNode(
             currentSection.id,
             0,
             newNode.getId(),
           );
-        const sourceNode = existingTrainRunSection.getSourceNode();
-        const targetNode = newTrainRunSection.getTargetNode();
-        const interpolatedPosition = new Vec2D(
-          sourceNode.getPositionX() + (targetNode.getPositionX() - sourceNode.getPositionX()) * 0.5,
-          sourceNode.getPositionY() + (targetNode.getPositionY() - sourceNode.getPositionY()) * 0.5,
-        );
-        newNode.setPosition(interpolatedPosition.getX(), interpolatedPosition.getY());
+
         currentSection = newTrainRunSection.getDto();
       }
     }

src/app/services/data/node.service.ts
@@ -254,8 +254,9 @@ export class NodeService implements OnDestroy {
     return node;
   }
 
-  addEmptyNode(): Node {
+  addEmptyNode(positionX: number, positionY: number): Node {
     const node: Node = new Node();
+    node.setPosition(positionX, positionY);
     node.setIsCollapsed(true);
     const resource: Resource = this.resourceService.createAndGetResource();
     node.setResourceId(resource.getId());