a77caa011d296833a17d10fb9630b82a4dfbc4cc
Clara Ni <clara.ni@outlook.fr>
2025-11-27 17:23:41 +0100

    feat(node): create/delete new nodes when adding/deleting stops
    When adding/deleting stops from the trainrun section panel, new empty nodes are
    created/deleted.
    If the user tries to delete nodes but there is no empty node left, then
    no node are deleted and a warning is displayed.
    
    Also fixing the tests accordingly.
    
    Signed-off-by: Clara Ni <clara.ni@outlook.fr>
    Signed-off-by: Louis Greiner <louis.greiner@proton.me>

--- src/app/view/editor-main-view/data-views/nodes.view.ts
+++ src/app/view/editor-main-view/data-views/nodes.view.ts
@@ -818,7 +818,6 @@ export class NodesView {
 
     this.editorView.trainrunSectionPreviewLineView.stopPreviewLine();
   }
-
   reconnectTransition(dragTransitionInfo: DragTransitionInfo, endNode: Node) {
     if (dragTransitionInfo.node.getId() === endNode.getId()) {
       this.editorView.trainrunSectionPreviewLineView.stopPreviewLine();
