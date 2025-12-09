b2a35cdd1ae04d950395f63f0cdf207f41e536b3
Louis Greiner <greiner.louis@gmail.com>
2025-12-09 16:29:11 +0100

    refactor(tsvo): introduce viewObject.getPosition()
    This reduces the code in trainrun section view.
    
    Signed-off-by: Louis Greiner <louis.greiner@proton.me>

--- src/app/view/editor-main-view/data-views/trainrunSectionViewObject.ts
+++ src/app/view/editor-main-view/data-views/trainrunSectionViewObject.ts
@@ -84,6 +84,10 @@ export class TrainrunSectionViewObject {
     return this.path[this.path.length - 1];
   }
 
+  getPosition(atSource: boolean): Vec2D {
+    return atSource ? this.getPositionAtSourceNode() : this.getPositionAtTargetNode();
+  }
+
   private generateKey(editorView: EditorView, trainrunSections: TrainrunSection[]): string {
     const firstSection = trainrunSections[0];
     const lastSection = trainrunSections.at(-1);
--- src/app/view/editor-main-view/data-views/trainrunsections.view.ts
+++ src/app/view/editor-main-view/data-views/trainrunsections.view.ts
@@ -156,10 +156,6 @@ export class TrainrunSectionsView {
     return arcGenerator();
   }
 
-  static getPosition(viewObject: TrainrunSectionViewObject, atSource: boolean): Vec2D {
-    return atSource ? viewObject.getPositionAtSourceNode() : viewObject.getPositionAtTargetNode();
-  }
-
   static getNode(trainrunSection: TrainrunSection, atSource: boolean): Node {
     return atSource ? trainrunSection.getSourceNode() : trainrunSection.getTargetNode();
   }
@@ -1245,18 +1241,15 @@ export class TrainrunSectionsView {
         d.getTrainrun().getId(),
       )
       .attr("d", (d: TrainrunSectionViewObject) => {
-        return TrainrunSectionsView.createSemicircle(
-          d,
-          TrainrunSectionsView.getPosition(d, atSource),
-        );
+        return TrainrunSectionsView.createSemicircle(d, d.getPosition(atSource));
       })
       .attr(
         "transform",
         (d: TrainrunSectionViewObject) =>
           "translate(" +
-          TrainrunSectionsView.getPosition(d, atSource).getX() +
+          d.getPosition(atSource).getX() +
           "," +
-          TrainrunSectionsView.getPosition(d, atSource).getY() +
+          d.getPosition(atSource).getY() +
           ")",
       )
       .attr(StaticDomTags.EDGE_NODE_ID, (d: TrainrunSectionViewObject) =>
@@ -1317,12 +1310,8 @@ export class TrainrunSectionsView {
       .attr(StaticDomTags.EDGE_LINE_LINE_ID, (d: TrainrunSectionViewObject) =>
         d.getTrainrun().getId(),
       )
-      .attr("cx", (d: TrainrunSectionViewObject) =>
-        TrainrunSectionsView.getPosition(d, atSource).getX(),
-      )
-      .attr("cy", (d: TrainrunSectionViewObject) =>
-        TrainrunSectionsView.getPosition(d, atSource).getY(),
-      )
+      .attr("cx", (d: TrainrunSectionViewObject) => d.getPosition(atSource).getX())
+      .attr("cy", (d: TrainrunSectionViewObject) => d.getPosition(atSource).getY())
       .attr("r", DEFAULT_PIN_RADIUS)
       .attr(StaticDomTags.EDGE_NODE_ID, (d: TrainrunSectionViewObject) =>
         TrainrunSectionsView.getNodeFromViewObject(d, atSource).getId(),
