49267c4452eb9c1d68008611b096cefa7538b8eb
Louis Greiner <greiner.louis@gmail.com>
2025-12-09 10:23:29 +0100

    drop unecessary path routing calls
    Signed-off-by: Louis Greiner <greiner.louis@gmail.com>

--- src/app/services/data/node.service.ts
+++ src/app/services/data/node.service.ts
@@ -944,7 +944,6 @@ export class NodeService implements OnDestroy {
     this.nodesStore.nodes.forEach((node) => {
       if (node.containsTrainrun(trainrun)) {
         node.updateTransitionsAndConnections();
-        this.trainrunSectionService.updateTrainrunSectionRouting(node, enforceUpdate);
       }
     });
   }
@@ -1162,14 +1161,12 @@ export class NodeService implements OnDestroy {
           .getPortOfTrainrunSection(port.getTrainrunSection().getId())
           .setPositionAlignment(portAlignments.targetPortPlacement);
         oppositeNode.updateTransitionsAndConnections();
-        this.trainrunSectionService.updateTrainrunSectionRouting(oppositeNode, enforceUpdate);
       });
       node.reorderAllPorts();
       this.operation.emit(new NodeOperation(OperationType.update, node));
     }
     node.updateTransitionsRouting();
     node.updateConnectionsRouting();
-    this.trainrunSectionService.updateTrainrunSectionRouting(node, enforceUpdate);
   }
 
   private findClearedLabel(node: Node, labelIds: number[]) {
