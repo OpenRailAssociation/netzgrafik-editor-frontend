65b153eccd134003cb6f64df9833356eb0801b51
Louis Greiner <greiner.louis@gmail.com>
2025-12-12 10:12:31 +0100

    fix(tsvo): tip display for collapsed nodes
    The filter feature on Tip could be refactored one day, as well as the rest of the filtering system
    
    Signed-off-by: Louis Greiner <louis.greiner@proton.me>

--- src/app/view/editor-main-view/data-views/trainrunsections.view.ts
+++ src/app/view/editor-main-view/data-views/trainrunsections.view.ts
@@ -1834,7 +1834,9 @@ export class TrainrunSectionsView {
       (trainrunSection: TrainrunSection) =>
         this.editorView.doCullCheckPositionsInViewport(
           SimpleTrainrunSectionRouter.computePath(trainrunSection),
-        ) && this.filterTrainrunSectionToDisplay(trainrunSection),
+        ) &&
+        this.filterTrainrunSectionToDisplay(trainrunSection) &&
+        !trainrunSection.areBothNodesCollapsed(),
     );
 
     const group = this.trainrunSectionGroup
@@ -2209,7 +2211,10 @@ export class TrainrunSectionsView {
     if (this.editorView.isTemporaryDisableFilteringOfItemsInViewEnabled()) {
       return true;
     }
-    return this.editorView.checkFilterNode(viewObject.getExtremityNode(atSource));
+    return (
+      this.editorView.checkFilterNode(viewObject.getExtremityNode(atSource)) &&
+      !viewObject.isTip(!atSource)
+    );
   }
 
   private transformPathAddExtraElementForPortAlignmentBottom(
@@ -2320,8 +2325,10 @@ export class TrainrunSectionsView {
     const trgNode = lastSection.getTargetNode();
     const path = viewObject.path;
 
-    let notFilteringSourceNode = this.editorView.checkFilterNode(srcNode);
-    let notFilteringTargetNode = this.editorView.checkFilterNode(trgNode);
+    let notFilteringSourceNode =
+      this.editorView.checkFilterNode(srcNode) && !viewObject.isTip(false);
+    let notFilteringTargetNode =
+      this.editorView.checkFilterNode(trgNode) && !viewObject.isTip(true);
 
     if (this.editorView.isTemporaryDisableFilteringOfItemsInViewEnabled()) {
       notFilteringSourceNode = true;
@@ -2357,15 +2364,19 @@ export class TrainrunSectionsView {
     return retPath;
   }
 
-  private filterOutAllTrainrunSectionWithHiddenNodeConnection(
-    trainrunSection: TrainrunSection,
+  private isFilteredOutAllTrainrunSectionWithHiddenNodeConnection(
+    viewObject: TrainrunSectionViewObject,
   ): boolean {
     if (this.editorView.isTemporaryDisableFilteringOfItemsInViewEnabled()) {
       return true;
     }
-    const filterSourceNode = this.editorView.checkFilterNode(trainrunSection.getSourceNode());
-    const filterTargetNode = this.editorView.checkFilterNode(trainrunSection.getTargetNode());
-    return filterSourceNode && filterTargetNode;
+    const isSourceNodeFiltered =
+      this.editorView.checkFilterNode(viewObject.firstSection.getSourceNode()) &&
+      !viewObject.isTip(false);
+    const isTargetNodeFiltered =
+      this.editorView.checkFilterNode(viewObject.lastSection.getTargetNode()) &&
+      !viewObject.isTip(true);
+    return isSourceNodeFiltered && isTargetNodeFiltered;
   }
 
   private oneNodeHiddenTrainrunSectionsRendering(
@@ -2376,7 +2387,7 @@ export class TrainrunSectionsView {
   ) {
     const groupLines = inGroupLines.filter(
       (d: TrainrunSectionViewObject) =>
-        !this.filterOutAllTrainrunSectionWithHiddenNodeConnection(d.firstSection),
+        !this.isFilteredOutAllTrainrunSectionWithHiddenNodeConnection(d),
     );
 
     this.make4LayerTrainrunSectionLines(
@@ -2393,7 +2404,7 @@ export class TrainrunSectionsView {
 
       const groupLabels = inGroupLabels.filter(
         (d: TrainrunSectionViewObject) =>
-          !this.filterOutAllTrainrunSectionWithHiddenNodeConnection(d.firstSection),
+          !this.isFilteredOutAllTrainrunSectionWithHiddenNodeConnection(d),
       );
 
       if (this.editorView.getLevelOfDetail() === LevelOfDetail.FULL) {
@@ -2500,7 +2511,7 @@ export class TrainrunSectionsView {
     inGroupLabels,
   ) {
     const groupLines = inGroupLines.filter((d: TrainrunSectionViewObject) =>
-      this.filterOutAllTrainrunSectionWithHiddenNodeConnection(d.firstSection),
+      this.isFilteredOutAllTrainrunSectionWithHiddenNodeConnection(d),
     );
 
     this.make4LayerTrainrunSectionLines(
@@ -2516,7 +2527,7 @@ export class TrainrunSectionsView {
       this.createAsymmetryArrows(groupLines, selectedTrainrun, connectedTrainIds, true);
 
       const groupLabels = inGroupLabels.filter((d: TrainrunSectionViewObject) =>
-        this.filterOutAllTrainrunSectionWithHiddenNodeConnection(d.firstSection),
+        this.isFilteredOutAllTrainrunSectionWithHiddenNodeConnection(d),
       );
 
       if (
