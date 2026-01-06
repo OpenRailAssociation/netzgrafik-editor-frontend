bcfcfd1b3e8e5f7bfaae3a3dba88ead467638cfe
Louis Greiner <greiner.louis@gmail.com>
2026-01-06 16:01:28 +0100

    chore: introduce TrainrunSectionText.TrainrunSectionBackwardTravelTime
    Co-authored-by: Simon Ser <contact@emersion.fr>
    Signed-off-by: Louis Greiner <greiner.louis@gmail.com>
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- src/app/services/data/trainrunsection.service.ts
+++ src/app/services/data/trainrunsection.service.ts
@@ -299,7 +299,7 @@ export class TrainrunSectionService implements OnDestroy {
 
   initializeTrainrunSectionRouting() {
     this.trainrunSectionsStore.trainrunSections.forEach((trainrunSection) => {
-      if (trainrunSection.isPathEmpty()) {
+      if (trainrunSection.isPathInvalid()) {
         trainrunSection.routeEdgeAndPlaceText();
       }
     });
