c2c8df5145a10d4915632ab9500ab5b5206d3e82
Simon Ser <contact@emersion.fr>
2026-01-09 17:37:49 +0100

    chore: introduce LeftAndRightSymmetryStructure
    Same as LeftAndRightLockStructure and LeftAndRightTimeStructure,
    but for symmetry.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- src/app/services/data/trainrun-section-times.service.ts
+++ src/app/services/data/trainrun-section-times.service.ts
@@ -13,6 +13,11 @@ import {TrainrunSection} from "../../models/trainrunsection.model";
 import {Node} from "../../models/node.model";
 import {LoadPerlenketteService} from "../../perlenkette/service/load-perlenkette.service";
 
+interface LeftAndRightSymmetryStructure {
+  leftSymmetry: boolean;
+  rightSymmetry: boolean;
+}
+
 const leftToRightStructureKeys = {
   tailDepartureTime: "leftDepartureTime",
   tailArrivalTime: "leftArrivalTime",
@@ -59,6 +64,11 @@ export class TrainrunSectionTimesService {
   private showWarningTwoLocks = false;
   private onLockButtonClicked = false;
 
+  private symmetryStructure: LeftAndRightSymmetryStructure = {
+    leftSymmetry: true,
+    rightSymmetry: true,
+  };
+
   private offset = 0;
   private offsetTransformationActive = false;
 
@@ -83,6 +93,10 @@ export class TrainrunSectionTimesService {
     );
     this.timeStructure = Object.assign({}, this.originalTimeStructure);
     this.lockStructure = this.trainrunSectionHelper.getLeftAndRightLock(
+      this.selectedTrainrunSection,
+      this.nodesOrdered,
+    );
+    this.symmetryStructure = this.trainrunSectionHelper.getLeftAndRightSymmetries(
       this.selectedTrainrunSection,
       this.nodesOrdered,
     );
