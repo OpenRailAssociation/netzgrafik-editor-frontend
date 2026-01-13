013d50e8603cd74069394d9be5af8c10feb376b8
Simon Ser <contact@emersion.fr>
2026-01-13 12:47:37 +0100

    test: add TrainrunSectionTimesService time update tests with lock
    Set the right lock, then update the left times or the travel time.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- src/app/services/data/trainrun-section-times.service.spec.ts
+++ src/app/services/data/trainrun-section-times.service.spec.ts
@@ -209,12 +209,54 @@ describe("TrainrunSectionTimesService", () => {
           travelTime: 20,
         },
       },
+      {
+        rightLock: true,
+        key: "leftDepartureTime" as const,
+        value: 15,
+        expectedTimeStructure: {
+          ...originalTimeStructure,
+          leftDepartureTime: 15,
+          leftArrivalTime: 45,
+          travelTime: 7,
+        },
+      },
+      {
+        rightLock: true,
+        key: "leftArrivalTime" as const,
+        value: 46,
+        expectedTimeStructure: {
+          ...originalTimeStructure,
+          leftDepartureTime: 14,
+          leftArrivalTime: 46,
+          travelTime: 8,
+        },
+      },
+      {
+        rightLock: true,
+        key: "travelTime" as const,
+        value: 20,
+        expectedTimeStructure: {
+          ...originalTimeStructure,
+          leftDepartureTime: 2,
+          leftArrivalTime: 58,
+          travelTime: 20,
+        },
+      },
     ];
 
-    for (const {key, value, expectedTimeStructure} of testCases) {
-      it(`set ${key} to ${value}`, () => {
+    for (const {rightLock, key, value, expectedTimeStructure} of testCases) {
+      const rightLockDesc = rightLock ? "with rightLock" : "";
+      it(`set ${key} to ${value} ${rightLockDesc}`, () => {
         const ts = trainrunSectionService.getTrainrunSectionFromId(trainrunSectionId);
         trainrunSectionTimesService.setTrainrunSection(ts);
+
+        // Apply the lock update, if any
+        if (rightLock) {
+          const lockStructure = trainrunSectionTimesService.getLockStructure();
+          lockStructure.travelTimeLock = false;
+          lockStructure.rightLock = true;
+          trainrunSectionTimesService.updateTrainrunSectionTimeLock();
+        }
 
         // Apply the time update
         const timeStructure = trainrunSectionTimesService.getTimeStructure();
