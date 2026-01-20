c7b439b7b0bfc0e8977ee2270db4e1095bf1e767
Louis Greiner <greiner.louis@gmail.com>
2026-01-20 16:51:25 +0100

    refactor(editor-main-view): add direction arg to extractTravelTime()
    This will be useful soon when we want to display the backward
    travel time.
    
    Co-authored-by: Simon Ser <contact@emersion.fr>
    Signed-off-by: Louis Greiner <greiner.louis@gmail.com>
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- src/app/view/editor-main-view/data-views/trainrunsections.view.spec.ts
+++ src/app/view/editor-main-view/data-views/trainrunsections.view.spec.ts
@@ -1241,48 +1241,56 @@ describe("TrainrunSection-View", () => {
     const v0 = TrainrunSectionsView.extractTravelTime(
       trainrunSectionService.getTrainrunSectionFromId(0),
       editorView,
+      "sourceToTarget",
     );
     expect(v0).toBe("10'");
 
     const v1 = TrainrunSectionsView.extractTravelTime(
       trainrunSectionService.getTrainrunSectionFromId(1),
       editorView,
+      "sourceToTarget",
     );
     expect(v1).toBe("10'");
 
     const v2 = TrainrunSectionsView.extractTravelTime(
       trainrunSectionService.getTrainrunSectionFromId(2),
       editorView,
+      "sourceToTarget",
     );
     expect(v2).toBe("20'");
 
     const v3 = TrainrunSectionsView.extractTravelTime(
       trainrunSectionService.getTrainrunSectionFromId(3),
       editorView,
+      "sourceToTarget",
     );
     expect(v3).toBe("49' (39')");
 
     const v4 = TrainrunSectionsView.extractTravelTime(
       trainrunSectionService.getTrainrunSectionFromId(4),
       editorView,
+      "sourceToTarget",
     );
     expect(v4).toBe("49' (10')");
 
     const v5 = TrainrunSectionsView.extractTravelTime(
       trainrunSectionService.getTrainrunSectionFromId(5),
       editorView,
+      "sourceToTarget",
     );
     expect(v5).toBe("51'");
 
     const v6 = TrainrunSectionsView.extractTravelTime(
       trainrunSectionService.getTrainrunSectionFromId(6),
       editorView,
+      "sourceToTarget",
     );
     expect(v6).toBe("10'");
 
     const v7 = TrainrunSectionsView.extractTravelTime(
       trainrunSectionService.getTrainrunSectionFromId(7),
       editorView,
+      "sourceToTarget",
     );
     expect(v7).toBe("10'");
   });
@@ -1296,7 +1304,7 @@ describe("TrainrunSection-View", () => {
     const t2 = n2.getTransition(ts.getId());
     t1.setIsNonStopTransit(true);
     t2.setIsNonStopTransit(true);
-    const v0 = TrainrunSectionsView.extractTravelTime(ts, editorView);
+    const v0 = TrainrunSectionsView.extractTravelTime(ts, editorView, "sourceToTarget");
     expect(v0).toBe("(10')");
   });
 
