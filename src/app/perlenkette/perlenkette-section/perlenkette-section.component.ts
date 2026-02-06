0f84d1e76cf860219d92649a23b1673592c4d4b3
Louis Greiner <greiner.louis@gmail.com>
2026-02-06 11:50:12 +0100

    refactor(perlenkette): drop show{Left,Right}DepartureTime()
    These functions are identical to showArrivalAndDepartureTime().
    
    Signed-off-by: Louis Greiner <greiner.louis@gmail.com>

--- src/app/perlenkette/perlenkette-section/perlenkette-section.component.ts
+++ src/app/perlenkette/perlenkette-section/perlenkette-section.component.ts
@@ -329,13 +329,6 @@ export class PerlenketteSectionComponent implements OnInit, AfterContentInit, On
     );
   }
 
-  showRightDepartureTime() {
-    if (this.filterService.isTemporaryDisableFilteringOfItemsInViewEnabled()) {
-      return true;
-    }
-    return this.filterService.isFilterArrivalDepartureTimeEnabled();
-  }
-
   /* right arrival time */
   getRightArrivalTime(): number {
     return this.roundTime(this.trainrunSectionTimesService.getTimeStructure().rightArrivalTime);
@@ -434,13 +427,6 @@ export class PerlenketteSectionComponent implements OnInit, AfterContentInit, On
       ) +
       (this.trainrunSection.hasTargetDepartureWarning() ? " " + StaticDomTags.TAG_WARNING : "")
     );
-  }
-
-  showLeftDepartureTime() {
-    if (this.filterService.isTemporaryDisableFilteringOfItemsInViewEnabled()) {
-      return true;
-    }
-    return this.filterService.isFilterArrivalDepartureTimeEnabled();
   }
 
   /* left arrival time */
