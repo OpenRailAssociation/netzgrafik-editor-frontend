0d87ff4b7e5d770d0bde72d537e3dec6b507d667
Alice K. <alice.khoudli@gmail.com>
2026-02-23 15:37:03 +0100

    feat: add missing TrainrunUpdateOperation events
    Signed-off-by: Alice K. <alice.khoudli@gmail.com>

--- src/app/services/data/trainrunsection.service.ts
+++ src/app/services/data/trainrunsection.service.ts
@@ -1101,6 +1101,13 @@ export class TrainrunSectionService implements OnDestroy {
     this.nodeService.connectionsUpdated();
     this.nodeService.nodesUpdated();
     this.trainrunSectionsUpdated();
+    this.operation.emit(
+      new TrainrunUpdateOperation(trainrunSection1.getTrainrun(), [
+        "nodes",
+        "times",
+        "numberOfStops",
+      ]),
+    );
   }
 
   setWarningOnNode(
