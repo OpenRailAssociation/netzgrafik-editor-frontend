01bb0cffe38939689dd26d0dfe30f239c7cd17d3
Simon Ser <contact@emersion.fr>
2026-03-04 10:42:11 +0100

    refactor(services): drop arrivalTimeAtTail in propagateTrainrunSectionTime()
    The name of the variable was confusing. Inline it since it's very
    short anyways.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- src/app/services/data/trainrunsection.service.ts
+++ src/app/services/data/trainrunsection.service.ts
@@ -420,7 +420,6 @@ export class TrainrunSectionService implements OnDestroy {
     previousSection: DirectedTrainrunSectionProxy,
     section: DirectedTrainrunSectionProxy,
   ) {
-    const arrivalTimeAtTail = previousSection.getHeadArrival();
     const halteZeit = this.getTrainrunSectionHaltezeit(
       previousSection.getHeadNode(),
       section.trainrunSection,
@@ -434,7 +433,7 @@ export class TrainrunSectionService implements OnDestroy {
 
     // Update tail arrival time
     const newTailDeparture = MathUtils.round(
-      (arrivalTimeAtTail + halteZeit) % 60,
+      (previousSection.getHeadArrival() + halteZeit) % 60,
       TrainrunSectionService.TIME_PRECISION,
     );
     const newTailArrival = MathUtils.round(
