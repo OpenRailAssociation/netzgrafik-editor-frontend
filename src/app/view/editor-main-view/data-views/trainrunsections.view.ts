4e3c8e67cdd4e6ca9e8f82ea54816a1c8f60bfed
Louis Greiner <louis.greiner@proton.me>
2026-03-12 11:56:52 +0100

    feat(view): enable node detail modal to be opened on clicking on a collapsed node
    Signed-off-by: Louis Greiner <louis.greiner@proton.me>

--- src/app/view/editor-main-view/data-views/trainrunsections.view.ts
+++ src/app/view/editor-main-view/data-views/trainrunsections.view.ts
@@ -1939,15 +1939,26 @@ export class TrainrunSectionsView {
     this.editorView.trainrunSectionPreviewLineView.updatePreviewLine(event);
   }
 
-  onCollapsedNodeMouseUp(event: MouseEvent, viewObject: TrainrunSectionViewObject) {
+  onCollapsedNodeMouseUp(
+    event: MouseEvent,
+    viewObject: TrainrunSectionViewObject,
+    stopIndex?: number,
+    numberOfStops?: number,
+  ) {
     event.stopPropagation();
-    if (this.editorView.editorMode === EditorMode.MultiNodeMoving) {
-      this.handleMultiNodeMovingTrainrunSectionMouseUp(event, viewObject.firstSection);
-      return;
-    }
     D3Utils.removeGrayout(viewObject);
     this.editorView.trainrunSectionPreviewLineView.stopPreviewLine();
     this.editorView.setTrainrunAsSelected(viewObject.getTrainrun());
+    if (
+      numberOfStops !== undefined &&
+      stopIndex !== undefined &&
+      numberOfStops <= SHOW_MAX_SINGLE_TRAINRUN_SECTIONS_STOPS
+    ) {
+      this.editorView.nodeService.setSingleNodeAsSelected(
+        viewObject.getCollapsedStopNodeFromStopIndex(stopIndex).getId(),
+      );
+      this.editorView.uiInteractionService.showNodeBaseData();
+    }
   }
 
   onTrainrunSectionTextMouseout(event: MouseEvent) {
@@ -2744,7 +2755,7 @@ export class TrainrunSectionsView {
         this.onCollapsedNodeMouseDown(event, t, stopIndex),
       )
       .on("mouseup", (event: MouseEvent, t: TrainrunSectionViewObject) =>
-        this.onCollapsedNodeMouseUp(event, t),
+        this.onCollapsedNodeMouseUp(event, t, stopIndex, numberOfStops),
       );
   }
 
