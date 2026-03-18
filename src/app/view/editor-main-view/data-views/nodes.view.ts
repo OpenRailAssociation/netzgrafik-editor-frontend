dbdc9d3dc446474529863c263b0dc5f95fcdaf52
Math_R_ <mathieu.richard747@gmail.com>
2026-03-18 17:23:14 +0100

    fix: fix which node is undocked when drag 6+ collapsed nodes dot
    -When there is more than 6+ collapsed node on the tsvo, the action
     of dragging the collapsed to another expanded node now undock
     the last collapsed node.

--- src/app/view/editor-main-view/data-views/nodes.view.ts
+++ src/app/view/editor-main-view/data-views/nodes.view.ts
@@ -809,7 +809,7 @@ export class NodesView {
   }
 
   replaceCollapsedNodeWithNode(dragCollapsedNodeInfo: any, endNode: Node) {
-    const draggedNode: Node = dragCollapsedNodeInfo.viewObject.getCollapsedStopNodeFromStopIndex(
+    const draggedNode: Node = dragCollapsedNodeInfo.viewObject.getCollapsedNodeToDrag(
       dragCollapsedNodeInfo.stopIndex,
     );
 
