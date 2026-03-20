4cd6d1be8aa4deda188bde684f03592138df322a
Math_R_ <mathieu.richard747@gmail.com>
2026-03-20 12:00:03 +0100

    fix(service, test): checkmissingtransitionsafterdeletion test
    -transition are not check anymore when the user add and
     delete stop from the trainrunsection tab component

--- src/app/view/editor-main-view/data-views/editor.keyEvents.ts
+++ src/app/view/editor-main-view/data-views/editor.keyEvents.ts
@@ -544,7 +544,7 @@ export class EditorKeyEvents {
         if (ts.getSourceNode().selected() && ts.getTargetNode().selected()) {
           newTrainrunSectionToModify.push(ts);
         } else {
-          this.trainrunSectionService.deleteTrainrunSection(ts.getId(), false);
+          this.trainrunSectionService.deleteTrainrunSection(ts.getId(), false, true);
         }
       });
     });
@@ -752,7 +752,7 @@ export class EditorKeyEvents {
     });
     visibleTrainrunSections = visibleTrainrunSections.filter((v, i, a) => a.indexOf(v) === i);
     visibleTrainrunSections.forEach((trainrunSectionId: number) => {
-      this.trainrunSectionService.deleteTrainrunSection(trainrunSectionId, false);
+      this.trainrunSectionService.deleteTrainrunSection(trainrunSectionId, false, true);
     });
 
     let selectedNodeDeleted = false;
