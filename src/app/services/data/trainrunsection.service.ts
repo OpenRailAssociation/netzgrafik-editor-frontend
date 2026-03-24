c39180821f2353c4daacd3905040bedb4f40d8f6
Math_R_ <mathieu.richard747@gmail.com>
2026-03-24 11:17:27 +0100

    fix(import legacy): improve the performance when converting imported legacy json to the new dto

--- src/app/services/data/trainrunsection.service.ts
+++ src/app/services/data/trainrunsection.service.ts
@@ -962,6 +962,7 @@ export class TrainrunSectionService implements OnDestroy {
     trainrunSectionId: number,
     nodeId: number,
     stopDuration?: number,
+    enforceUpdate = true,
   ) {
     const trainrunSection1 = this.getTrainrunSectionFromId(trainrunSectionId);
     if (
@@ -1083,10 +1084,12 @@ export class TrainrunSectionService implements OnDestroy {
 
     this.trainrunService.propagateConsecutiveTimesForTrainrun(trainrunSection1.getId());
 
-    this.nodeService.transitionsUpdated();
-    this.nodeService.connectionsUpdated();
-    this.nodeService.nodesUpdated();
-    this.trainrunSectionsUpdated();
+    if (enforceUpdate) {
+      this.nodeService.initPortOrdering();
+      this.nodeService.transitionsUpdated();
+      this.nodeService.connectionsUpdated();
+      this.trainrunSectionsUpdated();
+    }
     return {
       existingTrainrunSection: trainrunSection1,
       newTrainrunSection: trainrunSection2,
