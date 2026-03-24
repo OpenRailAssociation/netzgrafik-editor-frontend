826df7bb963c97c2d49d1f5e6b21de69586bb1f8
Math_R_ <mathieu.richard747@gmail.com>
2026-03-24 11:17:27 +0100

    fix(import legacy): improve the performance when converting imported legacy json to the new dto

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -144,11 +144,16 @@ export class DataService implements OnDestroy {
         const {newTrainrunSection} = this.trainrunSectionService.replaceIntermediateStopWithNode(
           currentSection.id,
           newNode.getId(),
+          undefined,
+          false,
         );
 
         currentSection = newTrainrunSection.getDto();
       }
     }
+    this.nodeService.transitionsUpdated();
+    this.nodeService.connectionsUpdated();
+    this.trainrunSectionService.trainrunSectionsUpdated();
   }
 
   insertCopyNetzgrafikDto(netzgrafikDto: NetzgrafikDto, enforceUpdate = true) {
