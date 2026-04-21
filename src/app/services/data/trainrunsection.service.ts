f724cb666d92bc5d438110f9fd3a1ed384e32b48
Math_R_ <mathieu.richard747@gmail.com>
2026-04-22 01:39:48 +0200

    refactor(trainrunsection): rename noDurationStop to zeroStopDuration replaceIntermediateStopWithNode
    Signed-off-by: Math_R_ <mathieu.richard747@gmail.com>

--- src/app/services/data/trainrunsection.service.ts
+++ src/app/services/data/trainrunsection.service.ts
@@ -964,7 +964,7 @@ export class TrainrunSectionService implements OnDestroy {
   replaceIntermediateStopWithNode(
     trainrunSectionId: number,
     nodeId: number,
-    stopDuration?: number,
+    zeroStopDuration: boolean = false,
     enforceUpdate = true,
   ) {
     const trainrunSection1 = this.getTrainrunSectionFromId(trainrunSectionId);
@@ -1012,6 +1012,7 @@ export class TrainrunSectionService implements OnDestroy {
       nodeIntermediate.getId(),
       trainrunSection1,
       trainrunSection2,
+      zeroStopDuration,
     );
     this.trainrunService.propagateConsecutiveTimesForTrainrun(trainrunSection1.getId());
 
@@ -1028,8 +1029,9 @@ export class TrainrunSectionService implements OnDestroy {
     travelTime1 = travelTime1 < 0 ? travelTime2 : travelTime1;
     travelTime2 = travelTime2 < 0 ? travelTime1 : travelTime2;
     const calculatedTravelTime = Math.min(travelTime1, travelTime2);
-    const halteZeit =
-      stopDuration ?? Math.min(minHalteZeitFromNode, Math.max(0, calculatedTravelTime - 2));
+    const halteZeit = zeroStopDuration
+      ? 0
+      : Math.min(minHalteZeitFromNode, Math.max(0, calculatedTravelTime - 2));
     const travelTimeIssue = !travelTime1 || !travelTime2;
     const halteZeitIssue = minHalteZeitFromNode < halteZeit;
     const travelTime = Math.max(trainrunSection1.getTravelTime() - halteZeit, 0);
@@ -1112,7 +1114,7 @@ export class TrainrunSectionService implements OnDestroy {
       interpolatedPosition.getY(),
     );
 
-    this.replaceIntermediateStopWithNode(trainrunSection.getId(), newNode.getId(), 0);
+    this.replaceIntermediateStopWithNode(trainrunSection.getId(), newNode.getId(), true);
     this.operation.emit(new TrainrunOperation(OperationType.update, trainrunSection.getTrainrun()));
   }
 
