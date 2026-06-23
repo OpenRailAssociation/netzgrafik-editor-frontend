bdbe7d448c64ecf55f867153ffea649e7671df96
MoritzWeber <kontakt@moritz-weber.net>
2026-06-23 13:23:22 +0200

    feat: Adding stretching and shortening of train sections

--- src/app/services/data/node.service.ts
+++ src/app/services/data/node.service.ts
@@ -1201,7 +1201,7 @@ export class NodeService implements OnDestroy {
     this.nodesStore.nodes = this.nodesStore.nodes.filter((n) => n.getId() !== nodeId);
   }
 
-  private changeNodePositionWithoutUpdate(
+  changeNodePositionWithoutUpdate(
     nodeId: number,
     newPositionX: number,
     newPositionY: number,
