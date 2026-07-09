bfc619cdfa36596feb5fee006a5a81556f03ec2c
Louis Greiner <louis.greiner@proton.me>
2026-07-09 17:55:28 +0200

    feat(service): add TimeStructure.numberOfStops property + move logic to helper
    Signed-off-by: Louis Greiner <louis.greiner@proton.me>

--- src/app/services/util/trainrunsection.helper.ts
+++ src/app/services/util/trainrunsection.helper.ts
@@ -23,7 +23,10 @@ export enum LeftAndRightElement {
 }
 
 export class TrainrunsectionHelper {
-  constructor(private trainrunService: TrainrunService) {}
+  constructor(
+    private trainrunService: TrainrunService,
+    private trainrunSectionService: TrainrunSectionService,
+  ) {}
 
   static getSymmetricTime(time: number) {
     return time === 0 ? 0 : 60 - time;
@@ -39,6 +42,7 @@ export class TrainrunsectionHelper {
       rightArrivalTime: 0,
       travelTime: 0,
       bottomTravelTime: 0,
+      numberOfStops: 0,
       stopTime: 0,
       bottomStopTime: 0,
     };
@@ -307,6 +311,7 @@ export class TrainrunsectionHelper {
         rightArrivalTime: section.getHeadArrival(),
         travelTime: section.getTravelTime(),
         bottomTravelTime: section.getReverseTravelTime(),
+        numberOfStops: 0,
         stopTime: 0,
         bottomStopTime: 0,
       };
@@ -365,6 +370,8 @@ export class TrainrunsectionHelper {
       rightArrivalTime: lastRightNode.getArrivalTime(rightTrainrunSection),
       travelTime: cumulativeTravelTime,
       bottomTravelTime: cumulativeBottomTravelTime,
+      numberOfStops:
+        this.trainrunSectionService.getTrainrunSectionGroupForSection(trainrunSection).length - 1,
       stopTime: MathUtils.mod60(totalForwardDuration - cumulativeTravelTime),
       bottomStopTime: MathUtils.mod60(totalBackwardDuration - cumulativeBottomTravelTime),
     };
