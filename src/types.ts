dab1abce531c68ab817ddb8608b17384d6a6b0bd
Simon Ser <contact@emersion.fr>
2026-07-22 09:57:02 +0200

    fix: add missing .js file extension to ESM imports
    ESM requires explicit file extensions:
    https://nodejs.org/api/esm.html#mandatory-file-extensions
    
    TypeScript doesn't add file extensions when producing .js files.
    This results in broken imports when trying to use NGE from Node.
    
    Fix this by adding explicit file extensions on the TypeScript side
    and enabling rewriteRelativeImportExtensions:
    https://www.typescriptlang.org/docs/handbook/release-notes/typescript-5-7.html#path-rewriting-for-relative-paths
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 import type {} from "@angular/localize/init";
 
-export * from "./app/data-structures/technical.data.structures";
-export * from "./app/data-structures/business.data.structures";
-export type * from "./app/models/operation.model";
+export * from "./app/data-structures/technical.data.structures.ts";
+export * from "./app/data-structures/business.data.structures.ts";
+export type * from "./app/models/operation.model.ts";
