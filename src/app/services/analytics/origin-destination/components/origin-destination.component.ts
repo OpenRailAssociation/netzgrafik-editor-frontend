OpenRailAssociation/netzgrafik-editor-frontend

commit c4e2103b91d14a97236b3ba0c0d767f4ff371144

chore(deps): upgrade d3 to v6
Migration guide: https://observablehq.com/@d3/d3v6-migration-guide

The following changes were required to migrate:

- Add an event argument to all .on() listeners, use it instead of the d3.event
  global
- Use event.currentTarget instead of a[i] in listeners (a new
  getMouseEventCurrentTarget() helper has been introduced to retain type
  safety)
- Replace d3.mouse() with d3.pointer() (this requires passing around the
  MouseEvent)

d3.drag() event listeners use the custom D3DragEvent event. The drag start
listener can access the current DOM element via event.sourceEvent.currentTarget.
However drag move and end listeners can't do that: currentTarget is set to the
Window object in that case. We need to store the DOM element when drag starts
to be able to access them during move/end.

Some event handlers passed a null domObj to functions down the stack (e.g.
hoverNode() and unhoverNode()). When domObj is non-null, these functions raise
and apply CSS class changes to the element. Add an explicit parameter to
control this behavior now that domObj is carried by the event parameter.

Signed-off-by: Simon Ser <contact@emersion.fr>

src/app/services/analytics/origin-destination/components/origin-destination.component.ts
@@ -272,7 +272,7 @@ export class OriginDestinationComponent implements OnInit, AfterViewInit, OnDest
 
     const nodeNameMap = new Map(nodeNames.map((n) => [n.id, n.fullName]));
 
-    const mousemove = function (d: OriginDestination) {
+    const mousemove = function (event: MouseEvent, d: OriginDestination) {
       let details = "";
       if (d.found) {
         details += "<br><hr> ";
@@ -286,11 +286,11 @@ export class OriginDestinationComponent implements OnInit, AfterViewInit, OnDest
           ${nodeNameMap.get(d.destinationId)} (<b>${d.destination}</b>)
           ${details}`,
         )
-        .style("left", `${d3.event.offsetX + 64}px`)
-        .style("top", `${d3.event.offsetY + 64 < 0 ? 0 : d3.event.offsetY + 64}px`);
+        .style("left", `${event.offsetX + 64}px`)
+        .style("top", `${event.offsetY + 64 < 0 ? 0 : event.offsetY + 64}px`);
     };
 
-    const mouseleave = function (_d: OriginDestination) {
+    const mouseleave = function (_event: MouseEvent, _d: OriginDestination) {
       tooltip.style("opacity", 0);
       d3.select(this)
         .style("stroke", "none")

src/app/view/editor-main-view/data-views/connections.view.ts
@@ -10,9 +10,12 @@ import {Vec2D} from "../../../utils/vec2D";
 import {ConnectionsViewObject} from "./connectionViewObject";
 import {LevelOfDetail} from "../../../services/ui/level.of.detail.service";
 
+type ConnectionDragEvent = d3.D3DragEvent<SVGElement, ConnectionsViewObject, unknown>;
+
 export class ConnectionsView {
   connectionsGroup: d3.Selection<SVGElement, undefined, Element, undefined>;
   editorView: EditorView;
+  dragDomObj: SVGElement | null = null;
 
   constructor(editorView: EditorView) {
     this.editorView = editorView;
@@ -139,14 +142,14 @@ export class ConnectionsView {
         (c: ConnectionsViewObject) => !ConnectionsView.displayConnection(c.connection, c.node),
       )
       .classed(StaticDomTags.TAG_SELECTED, (c: ConnectionsViewObject) => c.connection.selected())
-      .on("mouseover", (c: ConnectionsViewObject, i, a) =>
-        this.onConnectionMouseover(c.connection, a[i], c.node),
+      .on("mouseover", (event: MouseEvent, c: ConnectionsViewObject) =>
+        this.onConnectionMouseover(event, c.connection, c.node),
       )
-      .on("mouseout", (c: ConnectionsViewObject, i, a) =>
-        this.onConnectionMouseout(c.connection, a[i], c.node),
+      .on("mouseout", (event: MouseEvent, c: ConnectionsViewObject) =>
+        this.onConnectionMouseout(event, c.connection, c.node),
       )
-      .on("mouseup", (c: ConnectionsViewObject, i, a) =>
-        this.onConnectionMouseup(c.connection, a[i], c.node),
+      .on("mouseup", (event: MouseEvent, c: ConnectionsViewObject) =>
+        this.onConnectionMouseup(event, c.connection, c.node),
       );
   }
 
@@ -156,14 +159,14 @@ export class ConnectionsView {
   ) {
     const draggable = d3
       .drag<SVGElement, ConnectionsViewObject>()
-      .on("start", (cv: ConnectionsViewObject, i, a) =>
-        this.onConnectionPinDragStart(cv.connection, a[i]),
+      .on("start", (event: ConnectionDragEvent, cv: ConnectionsViewObject) =>
+        this.onConnectionPinDragStart(event, cv.connection),
       )
-      .on("drag", (cv: ConnectionsViewObject, i, a) =>
-        this.onConnectionPinDragged(cv.connection, a[i]),
+      .on("drag", (event: ConnectionDragEvent, cv: ConnectionsViewObject) =>
+        this.onConnectionPinDragged(event, cv.connection),
       )
-      .on("end", (cv: ConnectionsViewObject, i, a) =>
-        this.onConnectionPinDragEnd(cv.connection, a[i], cv.node),
+      .on("end", (event: ConnectionDragEvent, cv: ConnectionsViewObject) =>
+        this.onConnectionPinDragEnd(event, cv.connection, cv.node),
       );
 
     drawingGroup
@@ -180,14 +183,14 @@ export class ConnectionsView {
         ConnectionsView.getSelectedTrainrunId(cv.connection, cv.node),
       )
       .classed(StaticDomTags.TAG_SELECTED, (c: ConnectionsViewObject) => c.connection.selected())
-      .on("mouseover", (cv: ConnectionsViewObject, i, a) =>
-        this.onConnectionPinMouseover(cv.connection, a[i], cv.node),
+      .on("mouseover", (event: MouseEvent, cv: ConnectionsViewObject) =>
+        this.onConnectionPinMouseover(event, cv.connection, cv.node),
       )
-      .on("mouseout", (cv: ConnectionsViewObject, i, a) =>
-        this.onConnectionPinMouseout(cv.connection, a[i], cv.node),
+      .on("mouseout", (event: MouseEvent, cv: ConnectionsViewObject) =>
+        this.onConnectionPinMouseout(event, cv.connection, cv.node),
       )
-      .on("mouseup", (cv: ConnectionsViewObject, i, a) =>
-        this.onConnectionMouseup(cv.connection, a[i], cv.node),
+      .on("mouseup", (event: MouseEvent, cv: ConnectionsViewObject) =>
+        this.onConnectionMouseup(event, cv.connection, cv.node),
       )
       .call(draggable)
       .classed(StaticDomTags.TAG_WARNING, (cv: ConnectionsViewObject) =>
@@ -343,44 +346,50 @@ export class ConnectionsView {
     groupEnter: d3.Selection<SVGElement, ConnectionsViewObject, Element, undefined>,
   ) {}
 
-  onConnectionMouseup(connection: Connection, domObj: SVGElement, node: Node) {
-    d3.event.stopPropagation();
-    this.editorView.nodesView.handleMouseUpEvent(node);
+  onConnectionMouseup(event: MouseEvent, connection: Connection, node: Node) {
+    event.stopPropagation();
+    this.editorView.nodesView.handleMouseUpEvent(event, node);
   }
 
-  onConnectionMouseover(connection: Connection, domObj: SVGElement, node: Node) {
-    this.editorView.nodesView.hoverNodeDockable(node, null);
+  onConnectionMouseover(event: MouseEvent, connection: Connection, node: Node) {
+    this.editorView.nodesView.hoverNodeDockable(event, node);
   }
 
-  onConnectionMouseout(connection: Connection, domObj: SVGElement, node: Node) {
-    this.editorView.nodesView.unhoverNodeDockable(node, null);
+  onConnectionMouseout(event: MouseEvent, connection: Connection, node: Node) {
+    this.editorView.nodesView.unhoverNodeDockable(event, node);
   }
 
-  onConnectionPinMouseover(connection: Connection, domObj: SVGElement, node: Node) {
-    d3.select(domObj).classed(StaticDomTags.TAG_HOVER, true);
-    this.editorView.nodesView.hoverNodeDockable(node, null);
+  onConnectionPinMouseover(event: MouseEvent, connection: Connection, node: Node) {
+    d3.select(D3Utils.getMouseEventCurrentTarget(event)).classed(StaticDomTags.TAG_HOVER, true);
+    this.editorView.nodesView.hoverNodeDockable(event, node);
   }
 
-  onConnectionPinMouseout(connection: Connection, domObj: SVGElement, node: Node) {
-    d3.select(domObj).classed(StaticDomTags.TAG_HOVER, false);
-    this.editorView.nodesView.unhoverNodeDockable(node, null);
+  onConnectionPinMouseout(event: MouseEvent, connection: Connection, node: Node) {
+    d3.select(D3Utils.getMouseEventCurrentTarget(event)).classed(StaticDomTags.TAG_HOVER, false);
+    this.editorView.nodesView.unhoverNodeDockable(event, node);
   }
 
-  onConnectionPinDragStart(connection: Connection, domObj: SVGElement) {
+  onConnectionPinDragStart(event: ConnectionDragEvent, connection: Connection) {
+    const domObj = D3Utils.getMouseEventCurrentTarget(event.sourceEvent);
+    this.dragDomObj = domObj;
     d3.select(domObj).classed(StaticDomTags.CONNECTION_PIN_DRAGGING, true);
     D3Utils.disableTrainrunSectionForEventHandling();
   }
 
-  onConnectionPinDragged(connection: Connection, domObj: SVGElement) {
-    d3.select(domObj).classed(StaticDomTags.CONNECTION_PIN_DRAGGING, true);
-    const currentMousePosition = this.editorView.svgMouseController.getCurrentMousePosition();
-    const obj = d3.select(domObj);
+  onConnectionPinDragged(event: ConnectionDragEvent, connection: Connection) {
+    const obj = d3.select(this.dragDomObj);
+    obj.classed(StaticDomTags.CONNECTION_PIN_DRAGGING, true);
+    const currentMousePosition = this.editorView.svgMouseController.getCurrentMousePosition(
+      event.sourceEvent,
+    );
     obj.attr("cx", currentMousePosition.getX());
     obj.attr("cy", currentMousePosition.getY());
   }
 
-  onConnectionPinDragEnd(connection: Connection, domObj: SVGElement, node: Node) {
+  onConnectionPinDragEnd(event: ConnectionDragEvent, connection: Connection, node: Node) {
     D3Utils.resetTrainrunSectionForEventHandling();
+    const domObj = this.dragDomObj;
+    this.dragDomObj = null;
     d3.select(domObj).classed(StaticDomTags.CONNECTION_PIN_DRAGGING, false);
     if (this.editorView.nodesView.isNodeHovered(node)) {
       const obj = d3.select(domObj);

src/app/view/editor-main-view/data-views/d3.utils.ts
@@ -119,8 +119,9 @@ export class D3Utils {
       .classed(StaticDomTags.TAG_HOVER, false);
   }
 
-  static updateTrainrunSectionPreviewLine(startPos: Vec2D) {
-    const mousePos = d3.mouse(
+  static updateTrainrunSectionPreviewLine(event: MouseEvent, startPos: Vec2D) {
+    const mousePos = d3.pointer(
+      event,
       d3.select<SVGPathElement, undefined>(StaticDomTags.PREVIEW_LINE_DOM_REF).node(),
     );
     const mousePosVec: Vec2D = new Vec2D(mousePos[0], mousePos[1]);
@@ -141,8 +142,13 @@ export class D3Utils {
     );
   }
 
-  static updateIntermediateStopOrTransitionPreviewLine(startPos: Vec2D, endPos: Vec2D) {
-    const mousePos = d3.mouse(
+  static updateIntermediateStopOrTransitionPreviewLine(
+    event: MouseEvent,
+    startPos: Vec2D,
+    endPos: Vec2D,
+  ) {
+    const mousePos = d3.pointer(
+      event,
       d3.select<SVGPathElement, undefined>(StaticDomTags.PREVIEW_LINE_DOM_REF).node(),
     );
     const mousePosVec: Vec2D = new Vec2D(mousePos[0], mousePos[1]);
@@ -171,8 +177,13 @@ export class D3Utils {
     );
   }
 
-  static updateConnectionPreviewLine(startPos: Vec2D, canCombineTwoTrainrunsFlag: boolean) {
-    const mousePos = d3.mouse(
+  static updateConnectionPreviewLine(
+    event: MouseEvent,
+    startPos: Vec2D,
+    canCombineTwoTrainrunsFlag: boolean,
+  ) {
+    const mousePos = d3.pointer(
+      event,
       d3.select<SVGPathElement, undefined>(StaticDomTags.PREVIEW_CONNECTION_LINE_DOM_REF).node(),
     );
     const mousePosVec: Vec2D = new Vec2D(mousePos[0], mousePos[1]);
@@ -192,7 +203,7 @@ export class D3Utils {
     if (canCombineTwoTrainrunsFlag) {
       d3.selectAll(StaticDomTags.PREVIEW_CONNECTION_LINE_DOM_REF).classed(
         StaticDomTags.TAG_CTRLKEY,
-        d3.event.ctrlKey,
+        event.ctrlKey,
       );
     }
   }
@@ -918,5 +929,15 @@ export class D3Utils {
     return (
       prefix + (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes)
     );
+  }
+
+  static getMouseEventCurrentTarget(event: MouseEvent): SVGElement {
+    // Event is generic and isn't necessarily tied to the DOM. Unfortunately,
+    // MouseEvent doesn't narrow down currentTarget. We need to manually check
+    // its type.
+    if (!(event.currentTarget instanceof SVGElement)) {
+      throw new Error("Mouse event's current target is not an SVG element");
+    }
+    return event.currentTarget;
   }
 }

src/app/view/editor-main-view/data-views/editor.keyEvents.ts
@@ -61,27 +61,27 @@ export class EditorKeyEvents {
   activateMousekeyDownHandler(editorMode: EditorMode) {
     this.editorMode = editorMode;
 
-    d3.select("body").on("keyup", () => {
-      if (this.ignoreKeyEvent(d3.event)) {
+    d3.select("body").on("keyup", (event: KeyboardEvent) => {
+      if (this.ignoreKeyEvent(event)) {
         return;
       }
-      this.forwardCtrlKeyInformation();
+      this.forwardCtrlKeyInformation(event);
     });
 
-    d3.select("body").on("keydown", () => {
-      if (this.ignoreKeyEvent(d3.event)) {
+    d3.select("body").on("keydown", (event: KeyboardEvent) => {
+      if (this.ignoreKeyEvent(event)) {
         return;
       }
 
-      this.forwardCtrlKeyInformation();
+      this.forwardCtrlKeyInformation(event);
 
       if (this.trainrunSectionPreviewLineView.getMode() !== PreviewLineMode.NotDragging) {
-        d3.event.preventDefault();
+        event.preventDefault();
         return;
       }
 
-      const keycode = d3.event.code;
-      const ctrlKey = d3.event.ctrlKey;
+      const keycode = event.code;
+      const ctrlKey = event.ctrlKey;
       switch (keycode) {
         case "Delete":
           this.onKeyPressedDelete();
@@ -91,7 +91,7 @@ export class EditorKeyEvents {
           break;
         case "Escape":
           if (this.onKeyPressedEscape()) {
-            d3.event.preventDefault();
+            event.preventDefault();
           }
           break;
         case "KeyS":
@@ -100,59 +100,59 @@ export class EditorKeyEvents {
         case "KeyA":
           if (ctrlKey) {
             if (this.onSelectAll()) {
-              d3.event.preventDefault();
+              event.preventDefault();
             }
           }
           break;
         case "KeyC":
           if (ctrlKey) {
             if (this.onCopyAllVisibleElementsToCopyCache()) {
-              d3.event.preventDefault();
+              event.preventDefault();
             }
           }
           break;
         case "KeyV":
           if (ctrlKey) {
             if (this.onInsertAllVisibleElementsFromCopyCache()) {
-              d3.event.preventDefault();
+              event.preventDefault();
             }
           }
           break;
         case "KeyZ":
           if (ctrlKey) {
             if (this.onRevertLastChange()) {
-              d3.event.preventDefault();
+              event.preventDefault();
             }
           }
           break;
         case "KeyY":
           if (ctrlKey) {
             if (this.onRevertLastChange()) {
-              d3.event.preventDefault();
+              event.preventDefault();
             }
           }
           break;
         case "KeyD":
           if (ctrlKey) {
             this.onDuplicate();
-            d3.event.preventDefault();
+            event.preventDefault();
           }
           break;
         case "ArrowLeft":
           this.onArrowLeft();
-          d3.event.preventDefault();
+          event.preventDefault();
           break;
         case "ArrowUp":
           this.onArrowUp();
-          d3.event.preventDefault();
+          event.preventDefault();
           break;
         case "ArrowRight":
           this.onArrowRight();
-          d3.event.preventDefault();
+          event.preventDefault();
           break;
         case "ArrowDown":
           this.onArrowDown();
-          d3.event.preventDefault();
+          event.preventDefault();
           break;
 
         default:
@@ -337,9 +337,9 @@ export class EditorKeyEvents {
     this.trainrunSectionService.trainrunSectionsUpdated();
   }
 
-  private forwardCtrlKeyInformation() {
+  private forwardCtrlKeyInformation(event: KeyboardEvent) {
     const ctrlButtonState =
-      d3.event.ctrlKey && this.trainrunSectionPreviewLineView.canCombineTwoTrainruns();
+      event.ctrlKey && this.trainrunSectionPreviewLineView.canCombineTwoTrainruns();
 
     const obj1 = d3.selectAll(StaticDomTags.EDGE_LINE_PIN_DOM_REF);
     obj1.each(function () {

src/app/view/editor-main-view/data-views/editor.view.ts
@@ -590,8 +590,8 @@ export class EditorView implements SVGMouseControllerObserver {
     MultiSelectRenderer.setGroup(this.rootContainer);
   }
 
-  onEarlyReturnFromMousemove(): boolean {
-    return this.trainrunSectionPreviewLineView.updatePreviewLine();
+  onEarlyReturnFromMousemove(event: MouseEvent): boolean {
+    return this.trainrunSectionPreviewLineView.updatePreviewLine(event);
   }
 
   onStartMultiSelect() {
@@ -703,15 +703,19 @@ export class EditorView implements SVGMouseControllerObserver {
     }
   }
 
-  onGraphContainerMouseup(mousePosition: Vec2D, onPanning: boolean) {
+  onGraphContainerMouseup(event: MouseEvent, mousePosition: Vec2D, onPanning: boolean) {
     if (this.isMultiSelectOn) {
       return;
     }
     if (!this.trainrunSectionPreviewLineView.isDragging() && !onPanning) {
+      const domObj = event.target;
+      if (!(domObj instanceof SVGElement)) {
+        throw new Error("Mouse event's target is not an SVG element");
+      }
       if (
-        d3.event.button === 0 &&
+        event.button === 0 &&
         this.editorMode === EditorMode.TopologyEditing &&
-        d3.select(d3.event.target).attr("id") === EditorView.svgName
+        d3.select(domObj).attr("id") === EditorView.svgName
       ) {
         this.uiInteractionService.setEditorMode(EditorMode.NetzgrafikEditing);
         this.addNode(mousePosition.getX(), mousePosition.getY());
@@ -723,9 +727,9 @@ export class EditorView implements SVGMouseControllerObserver {
         this.uiInteractionService.closeNodeBaseData();
       }
       if (
-        d3.event.button === 0 &&
+        event.button === 0 &&
         this.editorMode === EditorMode.MultiNodeMoving &&
-        d3.select(d3.event.target).attr("id") === EditorView.svgName
+        d3.select(domObj).attr("id") === EditorView.svgName
       ) {
         this.unselectAllNodes();
         this.unselectAllNotes();
@@ -733,13 +737,13 @@ export class EditorView implements SVGMouseControllerObserver {
         this.uiInteractionService.setEditorMode(EditorMode.NetzgrafikEditing);
       }
       if (
-        d3.event.button === 0 &&
+        event.button === 0 &&
         this.editorMode === EditorMode.NoteEditing &&
-        d3.select(d3.event.target).attr("id") === EditorView.svgName
+        d3.select(domObj).attr("id") === EditorView.svgName
       ) {
         const clickPosition = new Vec2D(
-          d3.event.pageX + Note.DEFAULT_NOTE_WIDTH / 2,
-          d3.event.pageY + Note.DEFAULT_NOTE_HEIGHT / 2,
+          event.pageX + Note.DEFAULT_NOTE_WIDTH / 2,
+          event.pageY + Note.DEFAULT_NOTE_HEIGHT / 2,
         );
         this.addNote(mousePosition, clickPosition);
         this.uiInteractionService.setEditorMode(EditorMode.NetzgrafikEditing);

commit 63978e4203cdd5e502af1405e9fc0a39dd924c7f

fix(services): fix O/D matrix tooltip
It was permanently hidden because the first function parameter is
the event and has type "any". I missed this in the d3 v6 upgrade.

Signed-off-by: Simon Ser <contact@emersion.fr>
Fixes: c4e2103b91d1 ("chore(deps): upgrade d3 to v6")

src/app/services/analytics/origin-destination/components/origin-destination.component.ts
@@ -251,7 +251,7 @@ export class OriginDestinationComponent implements OnInit, AfterViewInit, OnDest
       .attr("class", "tooltip");
 
     // Three function that change the tooltip when user hover / move / leave a cell
-    const mouseover = function (d: OriginDestination) {
+    const mouseover = function (event: MouseEvent, d: OriginDestination) {
       if (d.found) {
         tooltip.style("opacity", 1);
         d3.select(this).style("stroke", "black").style("stroke-width", "2px").style("opacity", 1);