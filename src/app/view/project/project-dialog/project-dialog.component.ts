562602c87b4765e08deefb29a21dee2d65e06bd9
Simon Ser <contact@emersion.fr>
2026-09-15 11:13:16 +0200

    refactor(view): migrate ProjectDialogComponent to standalone component
    Same as previous commit.
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- src/app/view/project/project-dialog/project-dialog.component.ts
+++ src/app/view/project/project-dialog/project-dialog.component.ts
@@ -1,16 +1,27 @@
 import {Component, Inject, ChangeDetectionStrategy} from "@angular/core";
-import {SBB_DIALOG_DATA, SbbDialog, SbbDialogRef} from "@sbb-esta/angular/dialog";
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {SBB_DIALOG_DATA, SbbDialog, SbbDialogModule, SbbDialogRef} from "@sbb-esta/angular/dialog";
 import {ProjectFormComponentModel} from "./project-form/project-form.component";
+import {I18nModule} from "../../../core/i18n/i18n.module";
 import {FormModel} from "../../../utils/form-model";
 import {Observable} from "rxjs";
 import {filter} from "rxjs/operators";
+import {ProjectFormComponent} from "./project-form/project-form.component";
 
 @Component({
   selector: "sbb-project-dialog",
   templateUrl: "./project-dialog.component.html",
   styleUrls: ["./project-dialog.component.scss"],
   changeDetection: ChangeDetectionStrategy.Eager,
-  standalone: false,
+  imports: [
+    CommonModule,
+    I18nModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SbbDialogModule,
+    ProjectFormComponent,
+  ],
 })
 export class ProjectDialogComponent {
   readonly formModel: FormModel<ProjectFormComponentModel>;
